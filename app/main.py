7a32443207113d2413ef6b81533ceae3bb60b349
Ashutosh Narayan Jha <15161438+ashutoshnjha@users.noreply.github.com>
2023-03-25 18:39:17 +0530

    Update main.py
    Add hit end point

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
+from typing import Union
+
 from fastapi import FastAPI
+
 app = FastAPI()
 
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "anjLab"}
+    return {"Greetings": "AnjLab"}
+
+
+@app.get("/hit/{hit_id}")
+async def hit_count(hit_id: int, query: Union[str, None] = None):
+    return {"hit_id": hit_id, "query": query}
